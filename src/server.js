f7f95a616352607a2482a47309f9280af4571772
Lucas Pires <luc_50@outlook.com>
2020-06-04 16:42:51 -0300

    adicionando as rotas de conexão no servidor

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const express = require("express")
 const server = express()
 
+//Configurar pasta publica
+server.use(express.static("public"))
+
 //Configurar caminhos da aplicação
 //pagina inicial
 //req -requisição
 //res -resposta
 server.get("/", (req, res) => {
-    res.send("Hello World")
+    res.sendFile(__dirname + "/views/index.html")
+})
+server.get("/ceate-point", (req, res) => {
+    res.sendFile(__dirname + "/views/ceate-point.html")
 })
 
 //ligar o servidor
