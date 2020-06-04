ffacb36074a96d2aec21310e13313fcfeda0a3e5
Lucas Pires <luc_50@outlook.com>
2020-06-04 13:35:15 -0300

    configurando server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const express = require("express")
 const server = express()
 
+//Configurar caminhos da aplicação
+//pagina inicial
+//req -requisição
+//res -resposta
+server.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
 //ligar o servidor
 server.listen(3000)
